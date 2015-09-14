b8f407883d1c408c95918ab0aa72e37a0c3cd1ae
ryancheung <ryancheung.go@gmail.com>
2015-09-14 15:18:39 +0800

    Add updates/mangos/0001_locales_gossip_menu_option.sql

---
+++ updates/mangos/0001_locales_gossip_menu_option.sql
@@ -0,0 +1,5 @@
+DELETE FROM `locales_gossip_menu_option` WHERE `menu_id`=5065;
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc1`, `option_text_loc2`, `option_text_loc3`, `option_text_loc4`, `option_text_loc5`, `option_text_loc6`, `option_text_loc7`, `option_text_loc8`, `box_text_loc1`, `box_text_loc2`, `box_text_loc3`, `box_text_loc4`, `box_text_loc5`, `box_text_loc6`, `box_text_loc7`, `box_text_loc8`)
+VALUES
+	(5065, 0, NULL, NULL, NULL, '我想要一瓶水之精萃, 海达克西斯公爵', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+	(5065, 1, NULL, NULL, NULL, '我想要这个永恒精萃, 海达克西斯公爵', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
