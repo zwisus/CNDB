3428063623bc1f82c25e62d91b6be0c6decdb1c9
ryancheung <ryancheung.go@gmail.com>
2015-10-21 10:13:15 +0800

    Update updates/mangos/0002_locales_item.sql

--- updates/mangos/0002_locales_item.sql
+++ updates/mangos/0002_locales_item.sql
@@ -5,3 +5,4 @@ UPDATE `locales_item` SET `name_loc4`='极效法术石' WHERE `entry`=13603;
 UPDATE `locales_item` SET `name_loc4`='火焰石' WHERE `entry`=13699;
 UPDATE `locales_item` SET `name_loc4`='强效火焰石' WHERE `entry`=13700;
 UPDATE `locales_item` SET `name_loc4`='极效火焰石' WHERE `entry`=13701;
+UPDATE `locales_item` SET `name_loc4`='致盲粉' WHERE `entry`=5530;
