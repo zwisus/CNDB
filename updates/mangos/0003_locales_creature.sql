894ce635e190018ed9d03fb8b3f305d354c55eeb
ryancheung <ryancheung.go@gmail.com>
2015-11-15 21:30:12 +0800

    Update updates/mangos/0003_locales_creature.sql

--- updates/mangos/0003_locales_creature.sql
+++ updates/mangos/0003_locales_creature.sql
@@ -11,3 +11,5 @@ DELETE FROM `locales_creature` WHERE `entry`=17255;
 INSERT INTO `locales_creature` (`entry`, `name_loc1`, `name_loc2`, `name_loc3`, `name_loc4`, `name_loc5`, `name_loc6`, `name_loc7`, `name_loc8`, `subname_loc1`, `subname_loc2`, `subname_loc3`, `subname_loc4`, `subname_loc5`, `subname_loc6`, `subname_loc7`, `subname_loc8`)
 VALUES
 	(17255, '', '', '', '角鹰兽宝宝', '角鷹獸寶寶', '', '', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+UPDATE `locales_creature` SET `subname_loc4`='盗贼训练师' WHERE `subname_loc4`='潜行者训练师';
