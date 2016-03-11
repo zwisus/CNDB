59b6c3c8718d3e30a2c2630c59f7bb8114c04bf2
ryancheung <ryancheung.go@gmail.com>
2016-03-11 14:21:09 +0800

    Update updates/mangos/0005_locales_item.sql

--- updates/mangos/0005_locales_item.sql
+++ updates/mangos/0005_locales_item.sql
@@ -1,3 +1,4 @@
 -- Translation for item Deepstrider Tumor
 UPDATE `locales_item` SET `name_loc4`='深海巨人的肿瘤' WHERE `entry`=6082;
 UPDATE `locales_item` SET `name_loc4`='矮人火棍' WHERE `entry`=5241;
+UPDATE `locales_item` SET `description_loc4`=NULL WHERE `entry`=23199;
